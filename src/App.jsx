2c43b1988f641a43a3b9e563e8f5f3da2943bc34
Mauro Montero <93015917+canutoCanuto@users.noreply.github.com>
2022-03-30 16:18:56 -0300

    redirect al login si no hay token

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import Home from "./pages/Home";
 import Tables from "./pages/Tables";
 import Login from "./pages/Login";
 import { Route, Routes } from "react-router";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 function App() {
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (!user.token) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
   return (
     <div className="App">
       <Routes>
