1546fd8aebaa2d0a532519a804629fa184d16bbb
Luis Alvarez <96274912+Lraq1991@users.noreply.github.com>
2022-03-31 08:34:27 -0300

    Update Tables.jsx

--- src/pages/Tables.jsx
+++ src/pages/Tables.jsx
@@ -518,18 +518,6 @@ function ProductTable() {
               </div>
             </div>
           </main>
-          <footer className="py-4 bg-light mt-auto">
-            <div className="container-fluid px-4">
-              <div className="d-flex align-items-center justify-content-between small">
-                <div className="text-muted">Copyright &copy; Your Website 2022</div>
-                <div>
-                  <Link to="#">Privacy Policy</Link>
-                  &middot;
-                  <Link to="#">Terms &amp; Conditions</Link>
-                </div>
-              </div>
-            </div>
-          </footer>
         </div>
       </div>
     </div>
